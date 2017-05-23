d8204d404f6ad18aee9b27280cca66fbf54333bc
sergusha <serega.krutak@gmail.com>
2017-05-23 13:53:39 +0300

    pipe

--- src/test/java/com/sergusha/Runner/CucumberRunner.java
+++ src/test/java/com/sergusha/Runner/CucumberRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        format = {"pretty", "html:target/reports/html", "json:target/reports/mainReport.json"},
+        format = {"pretty", "json:target/reports/mainReport.json"},
         features = {"src/test/resources/features"},
         glue = {"com.sergusha.Stepdefs"} )
 public class CucumberRunner {
