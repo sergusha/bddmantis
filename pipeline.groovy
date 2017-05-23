8ef1bcb199eb3a9e6b91383599e027b7203db0e3
sergusha <serega.krutak@gmail.com>
2017-05-23 13:28:58 +0300

    pipeline config

---
+++ pipeline.groovy
@@ -0,0 +1,14 @@
+node {
+  git url: 'https://github.com/sergusha/bddmantis.git', branch: 'master'
+  def mvnHome = tool 'M3'
+  sh "mvn clean test"
+  step([$class: 'CucumberReportPublisher',
+        failedFeaturesNumber: 0,
+        failedScenariosNumber: 0,
+        failedStepsNumber: 0, fileExcludePattern: '',
+        fileIncludePattern: '**/*.json',
+        jsonReportDirectory: '',
+        pendingStepsNumber: 0,
+        skippedStepsNumber: 0,
+        undefinedStepsNumber: 0])
+}
